349144d5630d52f912fb5f358e55e61d552e0b18
willdevgh <willdev@outlook.com>
2018-04-01 14:01:54 +0800

    标题栏显示图片信息，函数名称改变

--- wallpaperbrowser.py
+++ wallpaperbrowser.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import os
+import tkinter.messagebox
 from tkinter import *
 import asyncio
 
@@ -24,23 +25,28 @@ class App:
         self._file_list = os.listdir(self._image_path)
         self._db = database
 
+        if not self._file_list:
+            tkinter.messagebox.showinfo('browser', '没找到墙纸！')
+            exit(0)
+
         self._image = Image.open(os.path.join(image_path, self._file_list[self._curr_index]))
         self._image.thumbnail((self._image.width/2, self._image.height/2))
         self._photo_image = ImageTk.PhotoImage(self._image)
 
         self._label = Label(master, image=self._photo_image)
-        self._label.bind("<Double-Button-1>", self.label_doubleclick)
+        self._label.bind("<Double-Button-1>", self.on_label_doubleclick)
         self._label.pack(side=TOP, expand=YES)
 
-        btn_prev = Button(master, text='上一张', fg='green', command=self.prev_image)
-        self._master.bind('<Left>', self.prev_image)
+        btn_prev = Button(master, text='上一张', fg='green', command=self.on_prev_image)
+        self._master.bind('<Left>', self.on_prev_image)
         btn_prev.pack(side=LEFT)
 
-        btn_next = Button(master, text='下一张', fg='green', command=self.next_image)
-        self._master.bind('<Right>', self.next_image)
+        btn_next = Button(master, text='下一张', fg='green', command=self.on_next_image)
+        self._master.bind('<Right>', self.on_next_image)
         btn_next.pack(side=RIGHT)
 
-        self._copyright_text = Message(master, text=self.get_copyright_text(self._file_list[self._curr_index]),
+        self._copyright_text = Message(master,
+                                       text=self.get_copyright(self._file_list[self._curr_index]),
                                        width=self._label.winfo_screenwidth())
         self._copyright_text.pack(side=LEFT)
         self._browse_progess_text = Message(master, text=self.get_browse_progress_text(), width=100)
@@ -49,12 +55,14 @@ class App:
         self._labelmenu_rightclick = Menu(master, tearoff=False)
         self._labelmenu_rightclick.add_command(label='打开路径', command=self.open_path)
         self._labelmenu_rightclick.add_command(label='重新下载（壁纸）', command=self.redownload)
-        master.bind("<Button-3>", self.label_rightclick)
+        master.bind("<Button-3>", self.on_label_rightclick)
 
-    def label_doubleclick(self, event):
+        self.set_title(self._curr_index)
+
+    def on_label_doubleclick(self, event):
         os.system(os.path.join(self._image_path, self._file_list[self._curr_index]))
 
-    def label_rightclick(self, event):
+    def on_label_rightclick(self, event):
         self._labelmenu_rightclick.post(event.x_root, event.y_root)
 
     def open_path(self):
@@ -62,7 +70,7 @@ class App:
 
     def redownload(self):
         curr_image = self._file_list[self._curr_index]
-        url = self._db.get_fullImageUrl(curr_image[:-4])
+        url = self._db.get_content_by_startdate(curr_image[:-4], 'fullImageUrl')
         full_name = os.path.join(self._image_path, curr_image)
 
         copyright_text = self._copyright_text['text']
@@ -77,7 +85,7 @@ class App:
         self._copyright_text.configure(text=copyright_text, fg='black')
         self._copyright_text.update()
 
-    def prev_image(self, event=None):
+    def on_prev_image(self, event=None):
         if 0 > self._curr_index - 1:
             self._curr_index = len(self._file_list) - 1
         else:
@@ -85,7 +93,7 @@ class App:
 
         self.update_image(self._curr_index)
 
-    def next_image(self, event=None):
+    def on_next_image(self, event=None):
         if len(self._file_list) <= self._curr_index + 1:
             self._curr_index = 0
         else:
@@ -101,15 +109,24 @@ class App:
         self._photo_image = ImageTk.PhotoImage(self._image)
         self._label.configure(image=self._photo_image)
         self._label.update_idletasks()
-        self._copyright_text.configure(text=self.get_copyright_text(curr_image))
+        self._copyright_text.configure(text=self.get_copyright(curr_image))
         self._browse_progess_text.configure(text=self.get_browse_progress_text())
+        self.set_title(index)
 
+    def set_title(self, index):
+        """在标题栏显示图片的日期信息"""
+        curr_image = self._file_list[index]
+        title_text = ''.join(['本地墙纸浏览', ' : ', curr_image[:-4], '~', self.get_enddate(curr_image)])
+        self._master.title(title_text)
 
-    def get_copyright_text(self, curr_image):
-        return self._db.get_copyright(curr_image[:-4])
+    def get_copyright(self, curr_image):
+        return self._db.get_content_by_startdate(curr_image[:-4], 'copyright')
+
+    def get_enddate(self, curr_image):
+        return self._db.get_content_by_startdate(curr_image[:-4], 'enddate')
 
     def get_browse_progress_text(self):
-        return "{}/{}".format(self._curr_index+1 , len(self._file_list))
+        return "{}/{}".format(self._curr_index+1, len(self._file_list))
 
     def run(self):
         self._master.mainloop()
@@ -130,7 +147,6 @@ def main():
         return
 
     root = Tk()
-    root.title("本地墙纸浏览")
     root.resizable(width=False, height=False)
 
     database = WallpaperDatabase(database_path)
