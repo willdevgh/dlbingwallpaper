e41a3b38fc381b6649d0984184e8bb9f671b0f53
willdevgh <willdev@outlook.com>
2020-04-04 19:18:42 +0800

    添加用于下载的方法

--- wallpaper_downloader.py
+++ wallpaper_downloader.py
@@ -4,11 +4,11 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import xml.etree.ElementTree as ET
+import json
+from collections import namedtuple
 
+# 3rd-party
 import requests
-from PyQt5.QtCore import QThread, pyqtSignal
-
-from utils import WallpaperDatabase
 
 rotating_file_handler = RotatingFileHandler(f'{__file__[:-3]}.log',
                                             maxBytes=1024 * 1024 * 4, backupCount=3, encoding='utf-8')
@@ -17,6 +17,10 @@ rotating_file_handler.setFormatter(logging.Formatter('%(asctime)s[%(levelname)s]
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(rotating_file_handler)
+
+IMAGES = 'images'
+STARTDATE, FULLSTARTDATE, ENDDATE, URL, COPYRIGHT = 'startdate', 'fullstartdate', 'enddate', 'url', 'copyright'
+ImageInfo = namedtuple('ImageInfo', [STARTDATE, FULLSTARTDATE, ENDDATE, URL, COPYRIGHT])
 
 
 class WallpaperDownloader(object):
@@ -28,19 +32,61 @@ class WallpaperDownloader(object):
     def host(self):
         return self.__host
 
-    def image_archive_url(self, idx=0, n=8) -> str:
+    def image_archive_url(self, start_date_index=0, day_count=8) -> str:
         """
         获取 HPImageArchive 的 URL
-        :param idx: 表示请求的日期索引(倒序): 0: 今天; 1: 昨天 ......
-        :param n: 表示请求的天数: 1: 今天当天; 2: 昨天和今天 ......
+        :param start_date_index: 表示请求的日期索引(倒序): 0: 今天; 1: 昨天 ......
+        :param day_count: 表示请求的天数: 1: 今天当天; 2: 昨天和今天 ......
         :return: url
         """
-        url = f"{self.__host}/HPImageArchive.aspx?format=js&idx={idx}&n={n}"
+        url = f"{self.__host}/HPImageArchive.aspx?format=js&idx={start_date_index}&n={day_count}"
         return url
+
+    def image_archive_dict(self, start_date_index=0, day_count=8) -> bytes:
+        """
+
+        :param start_date_index:
+        :param day_count:
+        :return:
+        """
+        res = requests.get(self.image_archive_url(start_date_index, day_count))
+        d = json.loads(s=res.content.decode(), encoding='utf-8')
+        return d
+
+    def image_info_list(self, start_date_index=0, day_count=8) -> list:
+        archive = self.image_archive_dict(start_date_index, day_count)
+        info_list = []
+        try:
+            images_info = archive[IMAGES]
+            for info in images_info:
+                info_list.append(ImageInfo(info[STARTDATE], info[FULLSTARTDATE], info[ENDDATE],
+                                           f"{self.__host}{info[URL]}", info[COPYRIGHT]))
+        except KeyError:
+            raise KeyError(f'keys: {IMAGES}, {STARTDATE}, {FULLSTARTDATE}, {ENDDATE}, {URL}, {COPYRIGHT}')
+
+        return info_list
+
+    @staticmethod
+    def download_image(image_url: str, save_file_name: str):
+        """
+        下载图片
+        :param image_url:
+        :param save_file_name:
+        :return:
+        """
+        image_data = requests.get(image_url, stream=True)
+        with open(save_file_name, 'wb') as image_file:
+            for chunk in image_data.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    image_file.write(chunk)
+                    image_file.flush()
 
 
 if __name__ == "__main__":
     # test
     downloader = WallpaperDownloader()
-    print(downloader.host)
-    print(downloader.image_archive_url(n=8))
+    info_list: list = downloader.image_info_list(day_count=8)
+    print(info_list)
+    for i, info in enumerate(info_list):
+        print(f"downloading: {info.copyright}")
+        downloader.download_image(info.url, f'{i}.jpg')
